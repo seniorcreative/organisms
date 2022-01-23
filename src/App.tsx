604aaae54b8bbf0c2bb75c972a6ca6a3c1bcb911
Steven Smith <steven.smith@sanitex-global.com>
2022-01-23 21:43:01 +1100

    Start building the data model, also put game controller in place.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import OrganismModel from "./organismModel";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const cellData = OrganismModel();
+  return <code className="text-left">{JSON.stringify(cellData)}</code>;
 }
 
 export default App;
---
+++ src/organismModel.tsx
@@ -0,0 +1,105 @@
+// HELLO I AM THE START OF THE ORGANISM DATA MODEL
+
+type OrganismNeighbours = {
+  TL: number;
+  T: number;
+  TR: number;
+  L: number;
+  R: number;
+  BL: number;
+  B: number;
+  BR: number;
+};
+
+type OrganismCell = {
+  x: number;
+  row: number;
+  col: number;
+  nbrs: OrganismNeighbours;
+};
+
+const OrganismModel = () => {
+  // Stage 1.
+  // Input of my algorithm worked out on pen and paper and graffiti
+  /*
+  TL | T | TR
+  L  | x | R
+  BL | B | BR
+  */
+  // TODO: Hook these up with organism state context
+  const ROWS: number = 5;
+  const COLS: number = 6;
+
+  const getTL = (row: number, col: number, x: number) =>
+    row === 1
+      ? col === 1
+        ? ROWS * COLS
+        : x - 1 + (ROWS - 1) * COLS
+      : x - 1 - COLS;
+
+  const getT = (row: number, col: number, x: number) =>
+    row === 1 ? x + (ROWS - 1) * COLS : x - COLS;
+
+  const getTR = (row: number, col: number, x: number) =>
+    row === 1
+      ? col === COLS
+        ? x - 1 + (ROWS - 1) * COLS
+        : x + 1 + (ROWS - 1) * COLS
+      : x + 1 - COLS;
+
+  const getL = (row: number, col: number, x: number) =>
+    col === 1 ? x - 1 + COLS : x - 1;
+
+  const getR = (row: number, col: number, x: number) =>
+    col === COLS ? x + 1 - COLS : x + 1;
+
+  const getBL = (row: number, col: number, x: number) =>
+    row === ROWS
+      ? col === 1
+        ? x - 1 - (ROWS - 1) * COLS + COLS
+        : x - 1 - (ROWS - 1) * COLS
+      : x - 1 + COLS;
+
+  const getB = (row: number, col: number, x: number) =>
+    row === ROWS ? x - (ROWS - 1) * COLS : x + COLS;
+
+  const getBR = (row: number, col: number, x: number) =>
+    row === ROWS
+      ? col === COLS
+        ? x - ROWS * COLS + 1
+        : x - (ROWS - 1) * COLS + 1
+      : x + COLS + 1;
+
+  const organismCellData: OrganismCell[] = [];
+
+  let row: number = 1;
+  let col: number = 1;
+  for (let x: number = 1; x <= ROWS * COLS; x++) {
+    const newOrganismCell: OrganismCell = {
+      row,
+      col,
+      x,
+      nbrs: {
+        TL: getTL(row, col, x),
+        T: getT(row, col, x),
+        TR: getTR(row, col, x),
+        L: getL(row, col, x),
+        R: getR(row, col, x),
+        BL: getBL(row, col, x),
+        B: getB(row, col, x),
+        BR: getBR(row, col, x),
+      },
+    };
+    organismCellData.push(newOrganismCell);
+    col++;
+    // After the end of each row of columns, wrap to a new line and start the column count again to build out the grid.
+    if ((col - 1) % COLS === 0) {
+      row++;
+      col = 1;
+    }
+  }
+
+  return organismCellData;
+};
+
+export default OrganismModel;
