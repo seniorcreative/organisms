6fdb7d02ace523dbc93e794529a289b87d50cd65
Steven Smith <steven.smith@sanitex-global.com>
2022-01-24 08:41:06 +1100

    Add first test for model for central cell neighbours

---
+++ src/organismModel.test.tsx
@@ -0,0 +1,16 @@
+import organismModel from "./organismModel";
+
+test("central cell neighbours", () => {
+  const organismCells = organismModel();
+  const cellNumber = 16;
+  expect(organismCells[cellNumber - 1].nbrs).toEqual({
+    TL: 9,
+    T: 10,
+    TR: 11,
+    L: 15,
+    R: 17,
+    BL: 21,
+    B: 22,
+    BR: 23,
+  });
+});
